d19f37ab08ba3072477fc5a1a1a460b5d6c230a6
sonalikasingh17 <singhsonalika5@gmail.com>
2025-11-04 19:29:19 +0530

    Started Phase 02 (Learn how to crop a image)

---
+++ Image_Resizing & Shaping/cropped.py
@@ -0,0 +1,15 @@
+import cv2
+
+image = cv2.imread("python_image.png")
+
+# Width, height
+
+if image is not None:
+    cropped = image[100:200, 50:150]
+
+    cv2.imshow("Original", image)
+    cv2.imshow("Cropped", cropped)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+else:
+    print("Could not load image")
